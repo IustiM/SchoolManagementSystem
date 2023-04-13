504b01deb357e8bb30bfcc90816e7f101a66a2b6
Mutescu Iustinian <iustinianmutescu@gmail.com>
2023-04-14 01:01:36 +0300

    accounts serializers

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,10 @@
-from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html', next_page='home'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
-    path('profile/', views.profile, name='profile'),
+    path('api/new_users/', NewUserList.as_view()),
+    path('api/new_users/<int:pk>/', NewUserDetail.as_view()),
+    path('api/managers/', CustomAccountManagerList.as_view()),
+    path('api/managers/<int:pk>/', CustomAccountManagerDetail.as_view()),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,49 +1,131 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.decorators import login_required
-from .forms import SignupForm, LoginForm
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-def signup(request):
-    if request.method == 'POST':
-        form = SignupForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('login')
-    else:
-        form = SignupForm()
-    return render(request, 'accounts/signup.html', {'form': form})
+from .models import NewUser
+from .serializers import NewUserSerializer
 
 
-def login_view(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('home')
-            else:
-                form.add_error(None, 'Invalid username or password')
-    else:
-        form = LoginForm()
-    return render(request, 'accounts/login.html', {'form': form})
+#
+# def signup(request):
+#     if request.method == 'POST':
+#         form = SignupForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data.get('username')
+#             raw_password = form.cleaned_data.get('password1')
+#             user = authenticate(username=username, password=raw_password)
+#             login(request, user)
+#             return redirect('login')
+#     else:
+#         form = SignupForm()
+#     return render(request, 'accounts/signup.html', {'form': form})
+#
+#
+# def login_view(request):
+#     if request.method == 'POST':
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             username = form.cleaned_data['username']
+#             password = form.cleaned_data['password']
+#             user = authenticate(request, username=username, password=password)
+#             if user is not None:
+#                 login(request, user)
+#                 return redirect('home')
+#             else:
+#                 form.add_error(None, 'Invalid username or password')
+#     else:
+#         form = LoginForm()
+#     return render(request, 'accounts/login.html', {'form': form})
+#
+# @login_required
+# def logout(request):
+#     return redirect('home')
+#
+#
+# def home(request):
+#     # response = HttpResponse('merge')
+#     # return response
+#     return render(request, 'accounts/home.html')
+#
+# def profile(request):
+#     return render(request, 'accounts/profile.html')
 
-@login_required
-def logout(request):
-    return redirect('home')
+
+class NewUserList(APIView):
+
+    def get(self, request, format=None):
+        new_users = NewUser.objects.all()
+        serializer = NewUserSerializer(new_users, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = NewUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-def home(request):
-    # response = HttpResponse('merge')
-    # return response
-    return render(request, 'accounts/home.html')
+class NewUserDetail(APIView):
 
-def profile(request):
-    return render(request, 'accounts/profile.html')
+    def get_object(self, pk):
+        return get_object_or_404(NewUser, pk=pk)
+
+    def get(self, request, pk, format=None):
+        new_user = self.get_object(pk)
+        serializer = NewUserSerializer(new_user)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        new_user = self.get_object(pk)
+        serializer = NewUserSerializer(new_user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        new_user = self.get_object(pk)
+        new_user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CustomAccountManagerList(APIView):
+
+    def get(self, request, format=None):
+        managers = NewUser.objects.all()
+        serializer = NewUserSerializer(managers, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = NewUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CustomAccountManagerDetail(APIView):
+
+    def get_object(self, pk):
+        return get_object_or_404(CustomAccountManagerDetail, pk=pk)
+
+    def get(self, request, pk, format=None):
+        manager = self.get_object(pk)
+        serializer = NewUserSerializer(manager.data)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        manager = self.get_object(pk)
+        serializer = NewUserSerializer(manager, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        manager = self.get_object(pk)
+        manager.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
